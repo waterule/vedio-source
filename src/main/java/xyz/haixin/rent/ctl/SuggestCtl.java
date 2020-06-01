60343eeeca9ba1e42c953ef4b470079977786270
lhy <waterule@163.com>
2020-06-01 14:38:10 +0800

    change the password

--- src/main/java/xyz/haixin/rent/ctl/SuggestCtl.java
+++ src/main/java/xyz/haixin/rent/ctl/SuggestCtl.java
@@ -10,6 +10,8 @@ import xyz.haixin.rent.mapper.VisitorMapper;
 import xyz.haixin.rent.vo.SuggestVo;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 @RestController
@@ -34,6 +36,28 @@ public class SuggestCtl {
     public String count(HttpServletRequest request){
 
         String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if(ip == null ){
+            ip = request.getRemoteAddr();
+        }
+        if(ip.equals("127.0.0.1") || ip.equals("0:0:0:0:0:0:0:1")){
+            //根据网卡取本机配置的IP
+            InetAddress inet=null;
+            try {
+                inet = InetAddress.getLocalHost();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            ip= inet.getHostAddress();
+        }
         Visitor visitor  = new Visitor();
         visitor.setIp(ip);
         List<Visitor> ip1 = visitorMapper.selectList(new QueryWrapper<Visitor>().eq("ip", ip));
