3c4bec210dff331d36b1669d95d96530e34408be
lhy <waterule@163.com>
2020-06-03 10:58:12 +0800

    文件上传

---
+++ src/main/java/xyz/haixin/rent/ctl/FileUploadCtl.java
@@ -0,0 +1,36 @@
+package xyz.haixin.rent.ctl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("file")
+public class FileUploadCtl {
+    @RequestMapping("/imageUpload.do")
+    public Map<String,Object> imageUpload(@RequestParam("file") MultipartFile multipartFile)  {
+        String fileSavePath="shoesImagePath";
+        if (null == multipartFile || multipartFile.getSize() <= 0) {
+            return new HashMap<String,Object>(){{put("code",400);put("msg","请选择上传文件。");}};
+        }
+        //文件名
+        String originalName = multipartFile.getOriginalFilename();
+        String fileName= UUID.randomUUID().toString().replace("-", "");
+        String picNewName = fileName + originalName.substring(originalName.lastIndexOf("."));
+        String imgRealPath = fileSavePath + picNewName;
+        try {
+            //保存图片-将multipartFile对象装入image文件中
+            File imageFile=new File(imgRealPath);
+            multipartFile.transferTo(imageFile);
+        } catch (Exception e) {
+            return new HashMap<String,Object>(){{put("code",400);put("msg","图片保存异常:"+e);}};
+        }
+        return new HashMap<String,Object>(){{put("code",200);put("msg",picNewName);}};
+    }
+}
